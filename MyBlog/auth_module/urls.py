018d85e138b03301bbd4d799f61d115ab9b14d86
Alexander <1906.kas@gmail.com>
2015-03-15 22:16:04 +0300

    Fix login realization

--- MyBlog/auth_module/urls.py
+++ MyBlog/auth_module/urls.py
@@ -4,7 +4,7 @@ from auth_module import views
 
 urlpatterns = patterns('',
 	url(r'^login/$', views.login_action, name='login'),
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.login_action, name='index'),
 	url(r'^registration/$', views.registration, name='registration'),
 	url(r'^logout/$', views.log_out_action, name='logout'),
 )
--- MyBlog/auth_module/views.py
+++ MyBlog/auth_module/views.py
@@ -3,25 +3,27 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-def index(request):
-	if request.user.is_authenticated():
-		return HttpResponse("You're already logged in!") 	
-	return render(request, 'auth_module/login.html')
 
 def login_action(request):
-	username = request.POST['login']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			return HttpResponse("You was successfuly logged in!") 
-	else:
-		return HttpResponse("Fail of login!")
-
+	if request.method == 'POST':
+		username = request.POST['login']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return HttpResponse("You was successfuly logged in!") 
+		else:
+			return HttpResponse("Fail of login!")
+	
+	if request.method == 'GET':
+		if request.user.is_authenticated():
+			return HttpResponse("You're already logged in!") 	
+		return render(request, 'auth_module/login.html')
 
 def registration(request):
-	return render(request, 'auth_module/registration.html')
+	if request.method == 'GET':
+		return render(request, 'auth_module/registration.html')
 
 def log_out_action(request):
 	logout(request)
