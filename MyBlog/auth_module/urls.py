e272c8f1230c0d6428f0e5dd653ab341ebfc313f
Alexander <1906.kas@gmail.com>
2015-03-15 22:32:38 +0300

    add registration

--- MyBlog/auth_module/urls.py
+++ MyBlog/auth_module/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from auth_module import views
 
 urlpatterns = patterns('',
-	url(r'^login/$', views.login_action, name='login'),
-	url(r'^$', views.login_action, name='index'),
-	url(r'^registration/$', views.registration, name='registration'),
-	url(r'^logout/$', views.log_out_action, name='logout'),
+                       url(r'^login/$', views.login_action, name='login'),
+                       url(r'^$', views.login_action, name='index'),
+                       url(r'^registration/$', views.registration, name='registration'),
+                       url(r'^logout/$', views.log_out_action, name='logout'),
 )
--- MyBlog/auth_module/views.py
+++ MyBlog/auth_module/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def login_action(request):
-	if request.method == 'POST':
-		username = request.POST['login']
-		password = request.POST['password']
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				return HttpResponse("You was successfuly logged in!") 
-		else:
-			return HttpResponse("Fail of login!")
-	
-	if request.method == 'GET':
-		if request.user.is_authenticated():
-			return HttpResponse("You're already logged in!") 	
-		return render(request, 'auth_module/login.html')
+    if request.method == 'POST':
+        username = request.POST['login']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse("You was successfuly logged in!")
+        else:
+            return HttpResponse("Fail of login!")
+
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            return HttpResponse("You're already logged in!")
+        return render(request, 'auth_module/login.html')
+
 
 def registration(request):
-	if request.method == 'GET':
-		return render(request, 'auth_module/registration.html')
+    if request.method == 'GET':
+        return render(request, 'auth_module/registration.html')
+    if request.method == 'POST':
+        username = request.POST['login']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username, email, password);
+        user.save()
+        return render(request, 'auth_module/login.html')
+
 
 def log_out_action(request):
-	logout(request)
-	return render(request, 'auth_module/login.html')
+    logout(request)
+    return render(request, 'auth_module/login.html')
